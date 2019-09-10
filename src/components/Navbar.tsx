bfdc83e5a03c0c28fa5778887279cd611d542184
Javier Pérez Arteaga <surtich@gmail.com>
2019-09-11 00:06:44 +0200

    [Navbar] Added logo Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.svg";
 
 export default class Navbar extends Component {
   render() {
-    return <div>Hello from navbar</div>;
+    return (
+      <nav className="navbar">
+        <Link to="/">
+          <img src={logo} alt="Beach Resort" />
+        </Link>
+      </nav>
+    );
   }
 }
