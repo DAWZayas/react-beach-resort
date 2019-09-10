4b26c7421d834b3c0876c540b73ca3f8889a8a59
Javier Pérez Arteaga <surtich@gmail.com>
2019-09-11 01:25:21 +0200

    [Navbar] Added links to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import { FaAlignRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.svg";
 
 export default class Navbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
   render() {
     return (
-      <nav className="navbar">
-        <Link to="/">
-          <img src={logo} alt="Beach Resort" />
-        </Link>
+      <nav className="nav">
+        <div className="nav__left">
+          <Link to="/">
+            <img src={logo} alt="Beach Resort" />
+          </Link>
+          <ul className={this.state.isOpen ? "nav__links-show" : "nav__links"}>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="nav__right">
+          <button
+            type="button"
+            className="nav__btn"
+            onClick={this.handleToggle}
+          >
+            <FaAlignRight className="nav__icon" />
+          </button>
+        </div>
       </nav>
     );
   }
