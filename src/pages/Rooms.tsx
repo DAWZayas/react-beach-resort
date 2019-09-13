fd945b0973585b0cb0c9707cea52ed92d203a97f
Javier Pérez Arteaga <surtich@gmail.com>
2019-09-13 16:06:58 +0200

    [Rooms] Implemented component

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Banner from "../components/Banner";
 import Hero from "../components/Hero";
+import RoomsContainer from "../containers/Rooms";
 
 const Rooms = () => {
   return (
-    <div data-testid="rooms-page">
-      <Hero hero="hero-rooms">
+    <>
+      <Hero hero="hero-rooms" data-testid="rooms-page">
         <Banner title="our rooms">
           <Link to="/" className="btn-primary">
             return home
           </Link>
         </Banner>
       </Hero>
-    </div>
+      <RoomsContainer />
+    </>
   );
 };
 
